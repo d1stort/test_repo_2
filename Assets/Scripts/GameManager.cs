76e749849ce012aec0acecdd4b72f810925559e4
Yana <yano4ca@gmail.com>
2016-03-05 19:08:12 +0300

    My commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector]
+    public bool playersTurn = true;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private bool doingSetup;
+    private int level = 1;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+	
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        doingSetup = true;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starverd.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (doingSetup)
+            return;
+	}
+}
